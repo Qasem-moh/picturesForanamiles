a59b58df93e6b73784394edaaa1f1e2e74d8efbe
qasem-moh <2171246@s.aou.edu.jo>
2021-06-22 21:34:06 +0300

    lab3

--- pictureanamiles/src/SelectedBeast.js
+++ pictureanamiles/src/SelectedBeast.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-
-import { Button, Modal } from 'react-bootstrap';
+import { Card, Button} from 'react-bootstrap'
 
 class SelectedBeast extends Component {
     constructor(props) {
@@ -15,64 +14,29 @@ class SelectedBeast extends Component {
             clicked: this.state.clicked + 1,
         });
     };
-    dialogImg(props) {
-
+   
+    render() {
+        // console.log(this.state);
         return (
-            <Modal
-                {...props}
-                size="lg"
-                aria-labelledby="contained-modal-title-vcenter"
-                centered
-            >
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Modal heading
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <h4>Centered Modal</h4>
-                    <p>
-                        Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                        dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                        consectetur ac, vestibulum at eros.
-                    </p>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={props.onHide}>Close</Button>
-                </Modal.Footer>
-            </Modal>
+            <Card style={{ width: '18rem' }} bg={'dark'} text={'white'}>
+                <Card.Img variant="top" src={this.props.image_url} />
+                <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Text>
+                        {this.props.description}
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            // <div id="divToRender">
+            //     <h2>{this.props.title}</h2>
+            //     <img src={this.props.image_url} alt={this.props.title} onClick={this.dialogImg} />
+            //     <p>{this.props.description}</p>
+            //     <button onClick={this.changeclicked}>click</button>
+            //     <h6>{this.state.clicked}</h6>
+            // </div>
         );
     }
-
-    App=()=> {
-        const [modalShow, setModalShow] = React.useState(false);
-
-        return (
-            <>
-                <Button variant="primary" onClick={() => setModalShow(true)}>
-                    Launch vertically centered modal
-                </Button>
-
-                <dialogImg
-                    show={modalShow}
-                    onHide={() => setModalShow(false)}
-                />
-            </>
-        );
-    }
-    // render() {
-    //     // console.log(this.state);
-    //     return (
-    //         <div id="divToRender">
-    //             <h2>{this.props.title}</h2>
-    //             <img src={this.props.image_url} alt={this.props.title} onClick={this.dialogImg} />
-    //             <p>{this.props.description}</p>
-    //             <button onClick={this.changeclicked}>click</button>
-    //             <h6>{this.state.clicked}</h6>
-    //         </div>
-    //     );
-    // }
-    render()
 }
 
 export default SelectedBeast;
